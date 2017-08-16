28cf0b7694d357bb4f5cf1194d7ad9988a46f5bc
Andrew Miller <ikari7789@yahoo.com>
2017-08-16 22:01:43 +0900

    Updated account creation redirect URLs.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/photo-submissions';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/photo-submissions';
 
     /**
      * Create a new controller instance.

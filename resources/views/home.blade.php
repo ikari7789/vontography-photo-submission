15550f4a2ec951af92b8d2ccb58ced3619842d33
Andrew Miller <ikari7789@yahoo.com>
2017-08-16 22:02:14 +0900

    Removed HomeController.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row mt-3 mb-3 justify-content-md-center">
         <div class="col-md-8 offset-md-2">
             <div class="card">
-                <div class="card-heading">Dashboard</div>
+                <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
                     You are logged in!
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('photos', 'PhotoController');
 Route::resource('photo-submissions', 'PhotoSubmissionController');
 Route::get('users', function() {

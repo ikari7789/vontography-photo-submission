6a3d24f1782f304f132cde21e3cbaf40a6c9dc4c
Andrew Miller <andrew.miller@rakuten.com>
2017-08-21 11:05:20 +0900

    Added missing dependency.

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use InvalidArgumentException;
 
 class PhotoController extends Controller

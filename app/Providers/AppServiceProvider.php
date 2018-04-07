b629fa5c46f050130acca85df23d53bc89224219
Andrew Miller <ikari7789@yahoo.com>
2018-04-08 02:00:49 +0900

    Remove fix for default string length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,9 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Fix MySQL error
-        // SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long; max key length is 767 byte
-        Schema::defaultStringLength(191);
+        //
     }
 
     /**
